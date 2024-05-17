23a4f0e71331b68eec7ac6a415adf2de9530c112
Harris0109 <mumeenaharris69@gmail.com>
2024-05-17 15:24:17 +0200

    Called methods from the system library

--- POE2/Program.cs
+++ POE2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* The following code will consist of 3 classes 2 used to store methods and 
+   and background code that will be used to gather user input these 2 classes 
+   are Ingredients and Recipe. The 3rd class is called Program which consistes of all
+   instructions and interactions used to engage with the user this is also known the interface.
+   Below the main method you will find 2 more backgorund methods used to add a new recipe and to display it.
+*/
+
+//Calling from the ssytem library in order to sanction the use of generic collections
+using System;
+using System.Collections.Generic;
+using System.Linq;
